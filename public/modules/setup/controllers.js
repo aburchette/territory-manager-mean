098c19bdb4d4de1735193c995609de14c4eec573
Alan Burchette <alanbur75@yahoo.com>
2015-02-06 16:17:25 -0500

    fixing routes

--- public/modules/setup/controllers.js
+++ public/modules/setup/controllers.js
@@ -4,4 +4,25 @@ angular.module('setup').
     controller('SetupCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
         $scope.params = $stateParams;
 
+    }]).
+    controller('AdminCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+        $scope.params = $stateParams;
+
+    }]).
+    controller('UsersCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+        $scope.params = $stateParams;
+
+    }]).
+    controller('MapCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+        $scope.params = $stateParams;
+
+    }]).
+    controller('StreetsCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+        $scope.params = $stateParams;
+
+    }]).
+    controller('UploadCtrl', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams){
+        $scope.params = $stateParams;
+
     }]);
+;
