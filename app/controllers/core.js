a16f62e9f471025f4789319e070176027dda03f4
Alan Burchette <alanburchette1@gmail.com>
2015-02-10 17:54:37 -0500

    updating routes and views

--- app/controllers/core.js
+++ app/controllers/core.js
@@ -1,10 +1,31 @@
 'use strict';
 
-/**
- * Module dependencies.
- */
-exports.index = function(req, res) {
-	res.render('index', {
-		user: req.user || null
-	});
+exports.app = function(req, res) {
+    res.render('app', {
+        user: req.user || null
+    });
+};
+
+exports.index = function(req, res){
+    res.render('index', {
+
+    });
+};
+
+exports.admin = function(req, res){
+    res.render('admin', {
+
+    });
+};
+
+exports.signup = function(req, res){
+    res.render('signup', {
+
+    });
+};
+
+exports.resetPassword = function(req, res){
+    res.render('reset-password', {
+
+    });
 };
